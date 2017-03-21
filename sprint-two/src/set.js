ff62d958060ac8c795e92c70b5051a697eaf7c9d
Jason Barnett <jason@thebarnetts.ws>
2017-03-21 17:24:47 -0500

    completes set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  this._storage = this._storage.filter(value => value !== item);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// add: O(n)
+// contains: O(n)
+// remove: O(n)
