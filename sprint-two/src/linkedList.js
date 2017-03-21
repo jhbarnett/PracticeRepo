8e91c47200b0cf12d0e0d0a211cf15979c6592c9
Jason Barnett <jason@thebarnetts.ws>
2017-03-21 14:04:09 -0500

    completes LinkedList and functional queue + stack

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+      this.tail = this.head;
+    } else {
+      list.tail.next = new Node(value);
+      list.tail = list.tail.next;
+    }    
   };
 
   list.removeHead = function() {
+    const result = this.head.value;
+    this.head = this.head.next;
+    return result;
   };
 
   list.contains = function(target) {
+    const search = (node, target) => {
+      if (node.value === target) {
+        return true;
+      } else if (node.next) {
+        return search(node.next, target);
+      } else {
+        return false;
+      }
+    }
+    return search(this.head, target)
   };
 
   return list;
@@ -27,3 +47,7 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ // addToTail: O(1)
+ // removeHead: O(1)
+ // contains: O(n)
