b2a9137a1a9ea2fe6933548639f6d9b6f7ad55dc
Jason Barnett <jason@thebarnetts.ws>
2017-03-21 15:24:44 -0500

    completes tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  Object.assign(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +12,21 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) { return true }
+  else {
+    return this.children.filter(child => 
+      child.contains(target)
+    ).length > 0;
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// addChild: O(1)
+// contains: O(n^2)
